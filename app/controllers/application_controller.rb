4dc1711d3019a7b93b300d4dfb479f36954ddc66
peppei20240408 <ippei.kanoh@gmail.com>
2024-08-14 19:45:23 +0900

    レビュアー修正依頼のリファクタリング

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,8 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name_kana])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name_kana])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:birth_date])
+    keys = [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date]
+    devise_parameter_sanitizer.permit(:sign_up, keys:)
   end
 
   def basic_auth
