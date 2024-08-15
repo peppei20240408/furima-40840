d93e139e29b90dc12294334ca0c33abb8bc47b43
peppei20240408 <ippei.kanoh@gmail.com>
2024-08-15 11:44:13 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,11 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :shipping_area
+  belongs_to :shipping_day
 end
