7b016f77991a7f89ecf96310453caced2374e0d4
peppei20240408 <ippei.kanoh@gmail.com>
2024-08-15 19:56:10 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,20 @@ class Item < ApplicationRecord
   belongs_to :delivery_charge
   belongs_to :shipping_area
   belongs_to :shipping_day
+
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :delivery_charge_id, presence: true
+  validates :shipping_area_id, presence: true
+  validates :shipping_day_id,  presence: true
+  validates :price, presence: true, numericality: { in: 300..9_999_999 }
+  validates :price, format: { with: /\A[0-9]+\z/ }
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_area_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
 end
