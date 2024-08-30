f6f1859e7687cb036a5c43156a12abd706ce7ed4
peppei20240408 <ippei.kanoh@gmail.com>
2024-08-30 22:20:41 +0900

    バリデーションの処理を実装

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -1,4 +1,15 @@
 class PurchaseShippingAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :shipping_area_id, :city, :address, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :user_id, :item_id, :postal_code, :shipping_area_id, :city, :address, :building_name, :phone_number
+
+  with_options presence: true do
+    validates :user_id, :item_id, :city, :address
+    validates :postal_code,  format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Input half-width numbers' }
+  end
+  validates :shipping_area, numericality: { other_than: 1, message: "can't be blank" }
+
+  def save
+    # 各テーブルにデータを保存する処理を書く
+  end
 end
