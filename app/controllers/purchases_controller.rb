7201fe1e22ebbfb83cd171620917593c17fbe5c5
peppei20240408 <ippei.kanoh@gmail.com>
2024-09-04 18:35:56 +0900

    フォームの情報がparamsで確認できた

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,10 @@
 class PurchasesController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
     @purchase_shipping_address = PurchaseShippingAddress.new
+  end
+
+  def create
+    binding.pry
   end
 end
