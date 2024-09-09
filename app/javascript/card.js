58a361acf600860119a8ef6784f8287afcd79963
peppei20240408 <ippei.kanoh@gmail.com>
2024-09-09 19:43:47 +0900

    公開鍵をPAY.JPに送る記述と、トークンが発行されたことを確認した

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  const payjp = Payjp('pk_test_***********************')
+  const payjp = Payjp('pk_test_33f0aa05565dedc566d1363e')
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
@@ -11,7 +11,15 @@ const pay = () => {
 
   const form = document.getElementById('charge-form')
   form.addEventListener("submit", (e) => {
-    console.log("フォーム送信時にイベント発火")
+    e.preventDefault();  // フォームのデフォルトの送信を防ぐ
+    payjp.createToken(numberElement).then(function (response) {
+      if (response.error) {
+      } else {
+        const token = response.id;
+        console.log(response)
+        debugger;
+      }
+    });
   });
 };
 
