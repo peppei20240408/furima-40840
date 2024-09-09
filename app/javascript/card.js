33b0d8660fa3888df6312f1de9b1b2aa0a500b61
peppei20240408 <ippei.kanoh@gmail.com>
2024-09-09 18:43:05 +0900

    取得したDOMからフォームに入力した値がJavaScript側で取得できること

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,17 @@
 const pay = () => {
+  const payjp = Payjp('pk_test_***********************')
+  const elements = payjp.elements();
+  const numberElement = elements.create('cardNumber');
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement = elements.create('cardCvc');
+
+  numberElement.mount('#number-form');
+  expiryElement.mount('#expiry-form');
+  cvcElement.mount('#cvc-form');
+
   const form = document.getElementById('charge-form')
   form.addEventListener("submit", (e) => {
-    const formData = new FormData(form)
-    
+    console.log("フォーム送信時にイベント発火")
   });
 };
 
