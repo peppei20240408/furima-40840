1b8f02d8d6ccc081c702611dad7f85b060181c2a
peppei20240408 <ippei.kanoh@gmail.com>
2024-09-10 17:07:44 +0900

    Revert "モデルのリファクタリング"
    This reverts commit b34d9c3498ade845c9a02521916ae16ced63266b.

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -3,7 +3,7 @@ class PurchaseShippingAddress
   attr_accessor :user_id, :item_id, :postal_code, :shipping_area_id, :city, :address, :building_name, :phone_number, :token
 
   with_options presence: true do
-    validates :user_id, :item_id, :city, :address, :token
+    validates :user_id, :item_id, :city, :address
     validates :postal_code,  format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Input half-width numbers' }
   end
